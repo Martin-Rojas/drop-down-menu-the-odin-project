7ddc2087349c1c6cff082b3e5fe41d992ca62b4c
Martin-Rojas <rojas.martin25@yahoo.com>
2025-08-04 18:59:33 -0700

    code toggle the dropdown menu

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import "./styles.css";
+
+const liProjects = document.getElementById(`projects`);
+const dropDownMenu = document.getElementById(`dropdown`);
+
+liProjects.addEventListener(`click`, () => {
+  dropDownMenu.classList.toggle("show");
+});
+
+// CLick outside the box to close the dropdown menu
