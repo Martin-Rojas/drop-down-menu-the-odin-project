0123691b9621ca5f265f670a2b399bf5d7824640
Martin-Rojas <rojas.martin25@yahoo.com>
2025-08-05 17:37:10 -0700

    code to close the dropdown menu

--- src/index.js
+++ src/index.js
@@ -3,8 +3,16 @@ import "./styles.css";
 const liProjects = document.getElementById(`projects`);
 const dropDownMenu = document.getElementById(`dropdown`);
 
-liProjects.addEventListener(`click`, () => {
+liProjects.addEventListener(`mouseover`, () => {
   dropDownMenu.classList.toggle("show");
 });
 
 // CLick outside the box to close the dropdown menu
+document.addEventListener("mouseover", (event) => {
+  const isClickInside =
+    liProjects.contains(event.target) || dropDownMenu.contains(event.target);
+
+  if (!isClickInside) {
+    dropDownMenu.classList.remove("show");
+  }
+});
